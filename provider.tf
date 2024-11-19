5341a3000b8ede30c7459f8911d6f66cade371d6
Sardhara Nitesh <sardharaniteshj@gmail.com>
2024-11-19 17:58:16 +0530

    1st commit

--- provider.tf
+++ provider.tf
@@ -3,11 +3,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.116.0"
+      version = "4.10.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "nitesh"                 # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "classicpipeline"        # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "nitesh"                 # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
- provider "azurerm" {
+provider "azurerm" {
+  subscription_id = "b806f439-4e29-426a-8a67-4ed653958018"
   features {
   }
 }
